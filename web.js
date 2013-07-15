c2874020f395da9371c3851fb8ee5a638ddefe75
Stephanie <ste.zhang@gmail.com>
2013-07-15 14:20:58 -0700

    Update web.js

--- web.js
+++ web.js
@@ -4,15 +4,6 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var fs = requires ('fs');
-  var content;
-  fs.readFileSync('./index.html', 'utf8', function(err, data) {
-       if (err) {
-           throw err;
-       }  
-       content = data;
-  });  
-  response.send(content);
-});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
